aa1f05fa283b5a05f0e833dec2b170c8d3f5b1a4
SaintRoge <lemondesansfil@gmail.com>
2016-12-21 11:58:55 +0100

    Window added

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,20 @@
 using namespace sf;
 
 int main(int argc, char const *argv[]) {
-	return 0;
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Tank");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
 }
